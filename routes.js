4fcce1030170f16c3f18216250b4af703e6b6aa9
Duartelitz <duartedigitalwork@gmail.com>
2024-06-15 16:34:36 -0300

    por THIADO / configuração do servidor

--- routes.js
+++ routes.js
@@ -1,4 +1,8 @@
 const express = require("express"); 
-const router = express.Router(); 
+const router = express.Router();   
+const {index} = require("./src/controllers/HomeController"); 
+//principal page route
+router.get("/", index);  
 
-router.get("/", index); 
+
+module.exports = router; 
--- server.js
+++ server.js
@@ -1,29 +1,35 @@
+//depedences
 const express = require("express"); 
 const mongoose = require("mongoose"); 
 const flash = require("connect-flash");     
 const  session = require("express-session");  
 const  MongoStore = require("connect-mongo"); 
-const {middlewareGlobal} = require("./src/middlewares/middlewareGlobal"); 
+const router = require("./routes"); 
 const path = require("path"); 
-require("dotenv").config(); 
-const app = express();  
+const {middlewareGlobal} = require("./src/middlewares/middlewareGlobal");  
+require("dotenv").config();  
 
-app.use(express.urlencoded({extended:true})); 
-app.use(express.static("public")); 
+const app = express();  //start express
+
+app.use(express.urlencoded({extended:true}));  //body parse configuration 
+//Configurações para  pasta de arquivos estaticos  saiba mais em :https://expressjs.com/en/starter/static-files.html#serving-static-files-in-express  
+app.use(express.static("public"));  
+//setando o motor para renderizar o nosso front-end
 app.set("view engine","ejs"); 
-app.set("views",path.resolve(__dirname,"src","views"))  
+app.set("views",path.resolve(__dirname,"src","views"))  //<- local onde o nosso HTML está
 
+//Conectando no Banco de Dados
 mongoose.connect(process.env.CONNECTION_URL).then(()=>{ 
-    console.log("Conectando..."); 
-    app.emit("Connected"); 
+    console.log("Conectando...");  
+    app.emit("Connected"); //quando estiver conectado, o express vai emitir um status conectado
 }) 
-
+//quando estiver conectado, o servidor vai estar escutando na porta que está no arquivo .env 
 app.on("Connected",()=>{
-    app.listen(process.env.PORT,()=>{
+    app.listen(process.env.PORT,()=>{ // o express vai ficar escutando tudo oque está  acontecendo na porta definida no arquivo .ev
       console.log("Conectado: http://localhost:3000/")      
     })
 })    
-
+//configuração da sessão :  https://www.npmjs.com/package/connect-mongo  e https://www.npmjs.com/package/express-session
 const sessionOptions = session( {
   secret: "Sessions Aplication", 
   resave: false, 
@@ -37,7 +43,9 @@ const sessionOptions = session( {
 })
 
 
+//usando as configurações  
 
 app.use(sessionOptions); 
-app.use(flash()); 
+app.use(flash());  
+app.use(router); 
 app.use(middlewareGlobal); 
