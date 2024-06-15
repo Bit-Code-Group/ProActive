0d66ea3670560f0441f488dbdea702931240c1bd
Dario Bastos <dario.bastos.dev@gmail.com>
2024-06-15 16:48:19 -0300

    Start Dário

--- routes.js
+++ routes.js
@@ -1,8 +1,11 @@
 const express = require("express"); 
 const router = express.Router();   
 const {index} = require("./src/controllers/HomeController"); 
+
 //principal page route
 router.get("/", index);  
+
+router.get()
 
 
 module.exports = router; 
