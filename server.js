27721fe5b570788afc44b424446579a202210926
Duartelitz <duartedigitalwork@gmail.com>
2024-06-15 16:22:50 -0300

    att

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express"); 
 const mongoose = require("mongoose"); 
-const flash = require("connect-flash");    
-const {middlewareGlobal} = require("src/middlewares/middlewareGlobal")
+const flash = require("connect-flash");     
+const  session = require("express-session");  
+const  MongoStore = require("connect-mongo"); 
+const {middlewareGlobal} = require("./src/middlewares/middlewareGlobal"); 
 const path = require("path"); 
 require("dotenv").config(); 
 const app = express();  
@@ -22,9 +24,20 @@ app.on("Connected",()=>{
     })
 })    
 
+const sessionOptions = session( {
+  secret: "Sessions Aplication", 
+  resave: false, 
+  saveUninitialized: false, 
+  store: MongoStore.create({mongoUrl:process.env.CONNECTION_URL}), 
+  cookie: {
+    httpOnly: true, 
+    maxAge: 1000* 60 * 7 *1000
+  } 
+
+})
 
 
 
+app.use(sessionOptions); 
 app.use(flash()); 
-
 app.use(middlewareGlobal); 
